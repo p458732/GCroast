76c529231fe935b73aa5abf56c36c6066911d2ed
佳豪 吳 <B10715023@gapps.ntust.edu.tw>
2019-12-15 03:05:49 +0800

    test

--- Roller Coaster/Shader/Square.fs
+++ Roller Coaster/Shader/Square.fs
@@ -1,13 +1,18 @@
 #version 430 core
 
 in vec2 vUV;
+in float colorChange;
 
 uniform sampler2D Texture;
-
 out vec4 fColor;
 
 void main()
-{
+{ 
 	
 	fColor = texture2D(Texture,vUV);
+	if(colorChange==0)
+	fColor = fColor-vec4(0.5,0.5,0.5,0);
+	else
+	fColor = fColor-vec4(colorChange,colorChange,colorChange,0);
+	
 }
--- Roller Coaster/Water.h
+++ Roller Coaster/Water.h
@@ -15,7 +15,7 @@
 #include <QString>
 
 
-class Square 
+class Water
 {
 public:
 	QOpenGLShaderProgram* shaderProgram;
@@ -27,30 +27,15 @@ public:
 	QOpenGLBuffer vvbo;
 	QOpenGLBuffer uvbo;
 public:
-	Square();
+	Water();
 	void Init();
 	void InitVAO();
 	void InitVBO();
-	void InitShader(QString vertexShaderPath,QString fragmentShaderPath);
-	void DimensionTransformation(GLfloat source[],GLfloat target[][4]);
+	void InitShader(QString vertexShaderPath, QString fragmentShaderPath);
+	void DimensionTransformation(GLfloat source[], GLfloat target[][4]);
 	void Begin();
 	void Paint(GLfloat* ProjectionMatrix, GLfloat* ModelViewMatrix);
 	void End();
 
-	float Divided = 30.0f;
-	float x1 = 230.0f, x2 = 270.0f, z1 = 215.0f, z2 = 275.0f;
-	float dx = (x2 - x1) / Divided, dz = (z2 - z1) / Divided;
-	float duv = 1.0f / Divided;
-	//wave
-	//振幅
-	float Amplitude = 2.0f;
-	//波長
-	float Wavelength = 1000.0f;
-	//速度
-	float Speed = 1.5f;
-	//基準高度
-	float H = 38.0f;
-	//time
-	float t;
 
 };
